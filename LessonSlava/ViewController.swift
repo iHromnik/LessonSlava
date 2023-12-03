7f30f136222602991dd014c08a99914d3d353d8a
Roman Khovalko <hromnik@me.com>
2023-12-03 13:25:23 +0200

    Initial Commit

---
+++ LessonSlava/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonSlava
+//
+//  Created by iHRomnik on 03.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
