61ef66a7b5d228b9134cc9f9089e30ec8feb1756
Roman Khovalko <hromnik@me.com>
2023-12-04 17:43:20 +0200

    Finish lesson-1

---
+++ LessonSlava/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  LessonSlava
+//
+//  Created by iHRomnik on 03.12.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    
+}
